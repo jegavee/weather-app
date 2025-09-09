18cc7cbfb97116cbdd218b60e2cd96cffbcfd344
Jeanne Gabrielle Veedor <jeanne.veedor@Paybps.local>
2025-09-09 13:18:24 +0800

    edited routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: '/home',
+        component: Home,
+        // redirectTo: '/home',
         // pathMatch: 'full'
     },
     // {
     //     path: 'home',
     //     loadComponent: () => import('./pages/home/home').then(c => c.Home)
-    // },
+    // }
     {
         path: '**',
         redirectTo: '',
