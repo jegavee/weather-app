ba9ca720fecf1a638ebe8736fda2e952c76995b1
Jeanne Gabrielle Veedor <jeanne.veedor@Paybps.local>
2025-09-09 13:14:59 +0800

    edited routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     {
         path: 'home',
         loadComponent: () => import('./pages/home/home').then(c => c.Home)
+    },
+    {
+        path: '**',
+        redirectTo: '',
     }
 ];
